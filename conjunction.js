70a4184435dca2a640788701f643b6a176a7a78a
Brittany Ramos-Janeway <bramosjaneway@gmail.com>
2019-01-03 12:15:14 -0600

    Completed mod squad scope assignment

---
+++ conjunction.js
@@ -0,0 +1,7 @@
+const conjunction = function (firstWord, secondWord) {
+    const conjoinedWord = `${firstWord} ${secondWord}`
+    return conjoinedWord
+}
+
+let newConjoinedWord = conjunction("Master", "Card")
+console.log(newConjoinedWord)
